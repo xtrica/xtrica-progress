5e413b0f1544583d4f75ac394a352645bb5cedd6
xtrica <xtrica.com@gmail.com>
2017-12-19 01:37:03 -0500

    v1.0.1513665423

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * xtrica-progress v1.0.1513665086 (https://xtrica.com)
+ * xtrica-progress v1.0.1513665423 (https://xtrica.com)
  * Copyright 2017 (or 2150?) Xtrica
  * Licensed under MIT
  */
@@ -12,20 +12,21 @@ if(!parent.appendChild(this.element)){document.body.appendChild(this.element)}}
 this.setProgressActive=!1
 this.setProgress=function(progress){let vm=this
 return new Promise(function(resolve,reject){vm.setProgressActive=!0
-if(!!vm.element){vm.progress=((parseInt(progress)>99)?100:((parseInt(progress)<0)?-1:parseInt(progress)))
+if(!!vm&&!!vm.timeout){clearTimeout(vm.timeout)}
+if(!!vm&&!!vm.element){vm.progress=((parseInt(progress)>99)?100:((parseInt(progress)<0)?-1:parseInt(progress)))
 if(vm.progress===-1){vm.element.style.opacity=0
-setTimeout(()=>{if(!!vm&&!!vm.element){vm.element.style.display='none'
+vm.timeout=setTimeout(()=>{if(!!vm&&!!vm.element){vm.element.style.display='none'
 vm.element.style.width='0'
 setTimeout(()=>{if(!!vm&&!!vm.element){vm.element.style.display='block'
 vm.setProgressActive=!1}
 resolve()},vm.speed)}else{resolve()}},vm.speed)}else{vm.element.style.opacity=1
 vm.element.style.display='block'
 vm.element.style.width=vm.progress+'%'
-setTimeout(()=>{if(!!vm){vm.setProgressActive=!1}
+vm.timeout=setTimeout(()=>{if(!!vm){vm.setProgressActive=!1}
 resolve()},vm.speed)}}else{vm.setProgressActive=!1
 resolve()}})}
 this.clear=()=>{let vm=this
-return new Promise(function(resolve,reject){if(vm.timeout){clearTimeout(vm.timeout)}
+return new Promise(function(resolve,reject){if(!!vm&&!!vm.timeout){clearTimeout(vm.timeout)}
 if(!!vm&&!!vm.element){vm.element.style.opacity=0
 setTimeout(()=>{resolve()},vm.speed)}else{resolve()}})}
 this.cleanup=()=>{let vm=this
